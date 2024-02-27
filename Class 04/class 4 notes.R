a6be80810308f112755a9dd721c71a9fba98b005
Farnam Tavakoli <Farnam.tavakoli97@gmail.com>
2024-02-27 11:52:26 -0800

    Class 4 Added

---
+++ Class 04/class 4 notes.R
@@ -0,0 +1,8 @@
+#my first R script
+
+x <- 1:50
+plot(x,sin(x))
+plot(x,sin(x), type="l")
+plot(x,sin(x), type="l", col="blue")
+plot(x,sin(x), type="l", col="blue", lwd=2)
+plot(x,sin(x), type="l", col="blue", lwd=2, xlab= "hello")
